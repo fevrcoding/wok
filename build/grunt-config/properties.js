21144550db8e22f8ce53cf08b40736b6eb3317ea
Marco Solazzi <marco.solazzi@intesys.it>
2014-09-05 10:50:33 +0200

    added browsersync support and plugin updates

--- build/grunt-config/properties.js
+++ build/grunt-config/properties.js
@@ -2,6 +2,8 @@
 
 
 module.exports = {
+    //set to true to enable browsersync (will override livereload task)
+    sync: false,
 	//set to `true` when paired with Phing
 	buildOnly: false,
 	viewmatch: '*.html' // for php projects use: '*.{html,php,phtml}'
