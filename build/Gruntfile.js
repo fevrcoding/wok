1716d05f9257acdc7d539a44bd48e763a7e65617
Marco Solazzi <marco.solazzi@intesys.it>
2014-10-30 14:35:32 +0100

    revving resorces in js files

--- build/Gruntfile.js
+++ build/Gruntfile.js
@@ -309,10 +309,18 @@ module.exports = function(grunt) {
                     concat: function (block) {
                         return '<script src="' + block.dest + '"></script>';
                     }
+                },
+                patterns: {
+                    // FIXME While usemin won't have full support for revved files we have to put all references manually here
+                    //see https://github.com/yeoman/grunt-usemin/issues/235#issuecomment-54136565
+                    js: [
+                        [/(\/.*?\.(?:gif|jpeg|jpg|png|webp|svg|css))/gm, 'Update the JS to reference our revved resouces']
+                    ]
                 }
             },
             html: ['<%= paths.usemin %>/**/<%= properties.viewmatch %>'],
-            css: ['<%= paths.css %>/{,*/}*.css']
+            css: ['<%= paths.css %>/{,*/}*.css'],
+            js: ['<%= paths.js %>/{,*/}*.js']
         },
 
 
