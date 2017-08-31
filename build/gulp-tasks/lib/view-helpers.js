9152c59eddbab61dc0942023fabd6c334daab1f9
Marco Solazzi <marco.solazzi+git@aquest.it>
2017-08-31 14:50:21 +0200

    es6 refactor

--- build/gulp-tasks/lib/view-helpers.js
+++ build/gulp-tasks/lib/view-helpers.js
@@ -9,14 +9,14 @@ const marked = require('marked');
 const Markdown = require('nunjucks-markdown/lib/markdown_tag');
 const loremIpsum = require('lorem-ipsum');
 
-module.exports.helpers = function (/*options*/) {
+module.exports.helpers = (/*options*/) => {
 
     return {
-        lorem: function (min, max, config) {
+        lorem(min, max, config) {
             const count = max ? random(min, max) : min;
             const loremDefaults = {
                 units: 'words',
-                count: count
+                count
             };
             const conf = defaults(config || {}, loremDefaults);
 
@@ -26,27 +26,27 @@ module.exports.helpers = function (/*options*/) {
 
 };
 
-module.exports.nunjucks = function (viewPath) {
+module.exports.nunjucks = (viewPath) => {
     const env = nunjucks.configure(viewPath, {
         noCache: true
     });
 
     const markdownTag = new Markdown(env, marked);
 
-    markdownTag.fileTag = function (context, file) {
+    markdownTag.fileTag = (context, file) => {
         return new nunjucks.runtime.SafeString(marked(env.render(file, context)));
     };
 
     // Markdown rendering for the block. Pretty simple, just get the body text and pass
     // it through the markdown renderer.
-    markdownTag.blockTag = function (context, bodFn, tabStartFn) {
+    markdownTag.blockTag = (context, bodFn, tabStartFn) => {
 
-        var body = bodFn(); //eslint-disable-line no-var
-        const tabStart = tabStartFn(); // The column postion of the {% markdown %} tag.
+        let body = bodFn(); //eslint-disable-line no-var
+        const tabStart = tabStartFn(); // The column options of the {% markdown %} tag.
 
         if (tabStart > 0) { // If the {% markdown %} tag is tabbed in, normalize the content to the same depth.
             body = body.split(/\r?\n/); // Split into lines.
-            // Subtract the column postion from the start of the string.
+            // Subtract the column options from the start of the string.
             body = body.map((line) => line.slice(tabStart));
             body = body.join('\n'); // Rejoin into one string.
         }
